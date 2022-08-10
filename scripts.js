0185b43005db18e69acce0be4f3047ecb7f487d2
L G <gozelm411@gmail.com>
2022-08-10 15:02:13 -0500

    change scripts

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function init () {
+    const takeOff = document.getElementById("takeoff");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    
+
+    takeOff.addEventListener('click', event => {
+     let response = window.confirm('Confirm that the shuttle is ready for takeoff');
+     if (response === true) {
+        paragraph.innerHTML = ('Shuttle in flight.');
+        shuttleBackground.target.style.backgroundColor = "blue";
+     }
+    });
+    
+    
+    
+}
+
+window.addEventListener("load", init);
