fe8d69e10c2ea1a41d6d6a811582ffe6282dc639
L G <gozelm411@gmail.com>
2022-08-11 08:45:32 -0500

    attempt 5

--- scripts.js
+++ scripts.js
@@ -5,6 +5,11 @@ function init () {
     const abortMission = document.getElementById("missionAbort");
     const paragraph = document.getElementById("flightStatus");
     const spaceShuttleHgt = document.getElementById("spaceShuttleHeight");
+    const upButton = document.getElementById("up");
+    const downButton = document.getElementById("down");
+    const rightButton = document.getElementById("right");
+    const leftButton = document.getElementById("left");
+    const moveRocket = document.getElementById("rocket");
     
 
     takeOff.addEventListener('click', event => {
@@ -18,18 +23,39 @@ function init () {
 
     landButton.addEventListener('click', event => {
         let resp = window.alert('Confirm that the shutThe shuttle is landing. Landing gear engaged.');
-          window.location = paragraph.innerHTML = ('The shuttle has landed.');
-           shuttleBackground.target.style.backgroundColor = "blue";
+          paragraph.innerHTML = ('The shuttle has landed.');
+          shuttleBackground.style.backgroundColor = "green";
+          spaceShuttleHgt.innerHTML -= parseInt(spaceShuttleHgt.innerHTML);
        });
 
     abortMission.addEventListener('click', event => {
         let res = window.confirm('Confirm that you want to abort the mission.');
         if (res === true) {
            paragraph.innerHTML = ('Mission aborted.');
-           shuttleBackground.target.style.backgroundColor = "green";
+           shuttleBackground.style.backgroundColor = "green";
+           spaceShuttleHgt.innerHTML -= parseInt(spaceShuttleHgt.innerHTML)
         }
-       });   
+    });   
     
+    upButton.addEventListener('click', event => {
+        spaceShuttleHgt.innerHTML = parseInt(spaceShuttleHgt.innerHTML) + 10000;
+    });
+    
+    downButton.addEventListener('click', event => {
+        spaceShuttleHgt.innerHTML = parseInt(spaceShuttleHgt.innerHTML) - 10000;
+    });
+
+    rightButton.addEventListener('click', event => {
+        moveRocket.style.position = 'absolute';
+        moveRocket.style.right = moveRocket.style.right + 10;
+    });
+
+    leftButton.addEventListener('click', event => {
+        moveRocket.style.position = 'absolute';
+        moveRocket.style.left = moveRocket.style.left + 10;
+    });
+       
+       
 }
 
 window.addEventListener("load", init);
